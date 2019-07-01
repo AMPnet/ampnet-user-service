a602f12a4edc96942888a389fa993592354938b9
Eugen Druzin <edruzin@gmail.com>
2019-07-01 11:57:03 +0200

    Change user primary key to uuid

--- src/main/kotlin/com/ampnet/userservice/persistence/model/User.kt
+++ src/main/kotlin/com/ampnet/userservice/persistence/model/User.kt
@@ -22,9 +22,6 @@ import javax.persistence.Table
 @Table(name = "app_user")
 data class User(
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Int,
-
     @Column
     var uuid: UUID,
 
