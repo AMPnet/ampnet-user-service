a5174a848a0d3c95288a11f35e8f644b073e6b72
Eugen Druzin <edruzin@gmail.com>
2020-07-22 18:14:26 +0200

    Fix web security filters

--- src/main/kotlin/com/ampnet/userservice/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/ampnet/userservice/config/WebSecurityConfig.kt
@@ -16,6 +16,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
@@ -69,6 +70,8 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
         val profileFilter = DisabledProfileFilter()
 
         http.cors().and().csrf().disable()
+            .formLogin().disable()
+            .httpBasic().disable()
             .logout().disable()
             .authorizeRequests()
             .antMatchers("/actuator/**").permitAll()
@@ -87,6 +90,6 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         http
             .addFilterBefore(authenticationTokenFilter, UsernamePasswordAuthenticationFilter::class.java)
-            .addFilterAfter(profileFilter, JwtAuthenticationFilter::class.java)
+            .addFilterAfter(profileFilter, FilterSecurityInterceptor::class.java)
     }
 }
