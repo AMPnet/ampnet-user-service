d9b9b569500c0885d893e53b248eccec26b1176b
Eugen Druzin <edruzin@gmail.com>
2020-10-22 11:41:31 +0200

    Set default coop

--- src/main/resources/db/migration/V5__coop.sql
+++ src/main/resources/db/migration/V5__coop.sql
@@ -7,8 +7,11 @@ CREATE TABLE coop(
     CONSTRAINT uc_host UNIQUE(host)
 );
 CREATE INDEX idx_coop_host ON coop(host);
+
+INSERT INTO coop VALUES ('ampnet', 'staging.ampnet.io', 'AMPnet', NOW(), null);
 ALTER TABLE app_user ADD COLUMN coop VARCHAR(64) NOT NULL DEFAULT 'ampnet';
 ALTER TABLE app_user ADD CONSTRAINT uc_email_in_coop UNIQUE(email, coop);
-CREATE INDEX idx_app_user_email_coop ON app_user(email, coop);
 
+CREATE INDEX idx_app_user_uuid_coop ON app_user(uuid, coop);
+CREATE INDEX idx_app_user_email_coop ON app_user(email, coop);
 DROP INDEX idx_app_user_role;
