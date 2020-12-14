cea3aad79792574c93839d68ba36c0e3c9fb578a
Eugen Druzin <edruzin@gmail.com>
2020-12-14 17:40:45 +0100

    Update mail-service.proto

--- src/main/kotlin/com/ampnet/userservice/grpc/mailservice/MailService.kt
+++ src/main/kotlin/com/ampnet/userservice/grpc/mailservice/MailService.kt
@@ -1,6 +1,6 @@
 package com.ampnet.userservice.grpc.mailservice
 
 interface MailService {
-    fun sendConfirmationMail(email: String, token: String, coop: String)
-    fun sendResetPasswordMail(email: String, token: String, coop: String)
+    fun sendConfirmationMail(request: UserDataWithToken)
+    fun sendResetPasswordMail(request: UserDataWithToken)
 }
