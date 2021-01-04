1a2b685dcc3894184a5c5aed210f8717ca6ac88c
Eugen Druzin <edruzin@gmail.com>
2021-01-04 14:14:33 +0100

    Add cache control for public controller

--- src/main/kotlin/com/ampnet/userservice/controller/PublicController.kt
+++ src/main/kotlin/com/ampnet/userservice/controller/PublicController.kt
@@ -7,14 +7,18 @@ import com.ampnet.userservice.exception.InternalException
 import com.ampnet.userservice.service.CoopService
 import com.ampnet.userservice.service.UserService
 import com.ampnet.userservice.service.pojo.CoopServiceResponse
+import mu.KLogging
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.http.CacheControl
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.Duration
 
 const val COOP_CACHE = "coop"
+const val CACHE_MAX_AGE_MINUTES = 10L
 
 @RestController
 class PublicController(
@@ -23,18 +27,27 @@ class PublicController(
     private val applicationProperties: ApplicationProperties
 ) {
 
+    companion object : KLogging()
+
+    private val cacheControl: CacheControl = CacheControl.maxAge(Duration.ofMinutes(CACHE_MAX_AGE_MINUTES))
+
     @GetMapping("/public/user/count")
     fun countRegisteredUsers(@RequestParam(required = false) coop: String?): ResponseEntity<RegisteredUsersResponse> {
         val registeredUsers = userService.countAllUsers(coop)
-        return ResponseEntity.ok(RegisteredUsersResponse(registeredUsers))
+        return ResponseEntity.ok()
+            .cacheControl(cacheControl)
+            .body(RegisteredUsersResponse(registeredUsers))
     }
 
     @GetMapping("/public/app/config/hostname/{hostname}")
     @Cacheable(value = [COOP_CACHE])
     fun getAppConfigByHostname(@PathVariable hostname: String): ResponseEntity<CoopServiceResponse> {
         coopService.getCoopByHostname(hostname)?.let {
-            return ResponseEntity.ok(it)
+            return ResponseEntity.ok()
+                .cacheControl(cacheControl)
+                .body(it)
         }
+        logger.info { "Missing coop config for hostname: $hostname" }
         return ResponseEntity.notFound().build()
     }
 
@@ -42,9 +55,12 @@ class PublicController(
     @Cacheable(value = [COOP_CACHE])
     fun getAppConfigByIdentifier(@PathVariable identifier: String): ResponseEntity<CoopServiceResponse> {
         coopService.getCoopByIdentifier(identifier)?.let {
-            return ResponseEntity.ok(it)
+            return ResponseEntity.ok()
+                .cacheControl(cacheControl)
+                .body(it)
         }
         val defaultCoop = applicationProperties.coop.default
+        logger.info { "Missing coop with identifier: $identifier. Returning default coop: $defaultCoop" }
         coopService.getCoopByIdentifier(defaultCoop)?.let {
             return ResponseEntity.ok(it)
         }
