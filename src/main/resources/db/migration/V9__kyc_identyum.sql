10b1d3ce9d187a126731b3c4e187e38ce344f96d
Eugen Druzin <edruzin@gmail.com>
2021-01-11 13:32:43 +0100

    Change kyc migration

--- src/main/resources/db/migration/V9__kyc_identyum.sql
+++ src/main/resources/db/migration/V9__kyc_identyum.sql
@@ -1,2 +1,2 @@
 ALTER TABLE user_info ADD COLUMN identyum_user_uuid VARCHAR;
-ALTER TABLE coop ADD COLUMN kyc_provider_id SMALLINT NOT NULL DEFAULT 0;
+ALTER TABLE coop ADD COLUMN kyc_provider_id INT NOT NULL DEFAULT 0;
