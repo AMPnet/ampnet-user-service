d31a9d85fa47c92b570d59ac8d735fab55fd1d10
Eugen Druzin <edruzin@gmail.com>
2021-03-17 15:05:27 +0100

    Cleanup

--- src/main/kotlin/com/ampnet/userservice/persistence/StringCryptoConverter.kt
+++ src/main/kotlin/com/ampnet/userservice/persistence/StringCryptoConverter.kt
@@ -4,17 +4,22 @@ import com.ampnet.userservice.exception.ErrorCode
 import com.ampnet.userservice.exception.InternalException
 import org.springframework.beans.factory.annotation.Value
 import java.security.GeneralSecurityException
+import java.security.InvalidAlgorithmParameterException
+import java.security.InvalidKeyException
+import java.security.Key
+import java.security.NoSuchAlgorithmException
 import java.util.Base64
 import javax.crypto.BadPaddingException
 import javax.crypto.Cipher
 import javax.crypto.IllegalBlockSizeException
+import javax.crypto.NoSuchPaddingException
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
 import javax.persistence.AttributeConverter
 import javax.persistence.Converter
 
 @Converter
 class StringCryptoConverter : AttributeConverter<String, String> {
-
-    private val cipherInitializer = CipherInitializer()
 
     @Value("\${spring.flyway.placeholders.private_key}")
     private lateinit var databaseEncryptionKey: String
@@ -23,7 +28,7 @@ class StringCryptoConverter : AttributeConverter<String, String> {
     override fun convertToDatabaseColumn(attribute: String?): String? {
         if (attribute.isNullOrBlank()) return attribute
         try {
-            val cipher = cipherInitializer.prepareAndInitCipher(Cipher.ENCRYPT_MODE, databaseEncryptionKey)
+            val cipher = createCipher(Cipher.ENCRYPT_MODE, databaseEncryptionKey)
             return encrypt(cipher, attribute)
         } catch (ex: GeneralSecurityException) {
             throw InternalException(ErrorCode.INT_ENCRYPTION, ex.localizedMessage)
@@ -34,7 +39,7 @@ class StringCryptoConverter : AttributeConverter<String, String> {
     override fun convertToEntityAttribute(dbData: String?): String? {
         if (dbData.isNullOrBlank()) return dbData
         try {
-            val cipher = cipherInitializer.prepareAndInitCipher(Cipher.DECRYPT_MODE, databaseEncryptionKey)
+            val cipher = createCipher(Cipher.DECRYPT_MODE, databaseEncryptionKey)
             return decrypt(cipher, dbData)
         } catch (ex: GeneralSecurityException) {
             throw InternalException(ErrorCode.INT_ENCRYPTION, ex.localizedMessage)
@@ -54,4 +59,15 @@ class StringCryptoConverter : AttributeConverter<String, String> {
         val decryptedBytes = cipher.doFinal(encryptedBytes)
         return String(decryptedBytes)
     }
+
+    @Throws(
+        InvalidAlgorithmParameterException::class, InvalidKeyException::class,
+        NoSuchAlgorithmException::class, NoSuchPaddingException::class
+    )
+    private fun createCipher(encryptionMode: Int, key: String): Cipher =
+        Cipher.getInstance("AES/CBC/PKCS5Padding").apply {
+            val secretKey: Key = SecretKeySpec(key.toByteArray(), "AES")
+            val algorithmParameters = IvParameterSpec(ByteArray(this.blockSize))
+            init(encryptionMode, secretKey, algorithmParameters)
+        }
 }
