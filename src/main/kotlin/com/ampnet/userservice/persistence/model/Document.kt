a31c0da2a00c91592a13b4a6e9e23f3cce125ccf
Eugen Druzin <edruzin@gmail.com>
2021-04-01 21:46:36 +0200

    Revert "Remove UserInfo encryption"
    This reverts commit 17b9c8857c2aa20a61727abefb82e3cfda16e1ee.

--- src/main/kotlin/com/ampnet/userservice/persistence/model/Document.kt
+++ src/main/kotlin/com/ampnet/userservice/persistence/model/Document.kt
@@ -1,14 +1,26 @@
 package com.ampnet.userservice.persistence.model
 
+import com.ampnet.userservice.persistence.StringCryptoConverter
 import com.ampnet.userservice.service.pojo.VeriffDocument
+import javax.persistence.Convert
 import javax.persistence.Embeddable
 
 @Embeddable
 class Document(
+
+    @Convert(converter = StringCryptoConverter::class)
     var type: String?,
+
+    @Convert(converter = StringCryptoConverter::class)
     var number: String?,
+
+    @Convert(converter = StringCryptoConverter::class)
     var country: String?,
+
+    @Convert(converter = StringCryptoConverter::class)
     var validUntil: String?,
+
+    @Convert(converter = StringCryptoConverter::class)
     var validFrom: String?
 ) {
     constructor(veriffDocument: VeriffDocument) : this(
