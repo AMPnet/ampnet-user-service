676ae251d68b51c2242eaca114d7f9dfefcb6dd0
Eugen Druzin <edruzin@gmail.com>
2021-04-02 12:33:57 +0200

    Change user info encryption key to Base64

--- src/main/kotlin/com/ampnet/userservice/persistence/StringCryptoConverter.kt
+++ src/main/kotlin/com/ampnet/userservice/persistence/StringCryptoConverter.kt
@@ -9,9 +9,7 @@ import java.security.InvalidKeyException
 import java.security.Key
 import java.security.NoSuchAlgorithmException
 import java.util.Base64
-import javax.crypto.BadPaddingException
 import javax.crypto.Cipher
-import javax.crypto.IllegalBlockSizeException
 import javax.crypto.NoSuchPaddingException
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
@@ -22,14 +20,16 @@ import javax.persistence.Converter
 class StringCryptoConverter : AttributeConverter<String, String> {
 
     @Value("\${spring.flyway.placeholders.private_key}")
-    private lateinit var databaseEncryptionKey: String
+    private lateinit var databaseEncryptionKeyInBase64: String
 
     @Throws(InternalException::class)
     override fun convertToDatabaseColumn(attribute: String?): String? {
         if (attribute.isNullOrBlank()) return attribute
         try {
-            val cipher = createCipher(Cipher.ENCRYPT_MODE, databaseEncryptionKey)
-            return encrypt(cipher, attribute)
+            val cipher = createCipher(Cipher.ENCRYPT_MODE)
+            val bytesToEncrypt = attribute.toByteArray()
+            val encryptedBytes = cipher.doFinal(bytesToEncrypt)
+            return Base64.getEncoder().encodeToString(encryptedBytes)
         } catch (ex: GeneralSecurityException) {
             throw InternalException(ErrorCode.INT_ENCRYPTION, ex.localizedMessage)
         }
@@ -39,34 +39,23 @@ class StringCryptoConverter : AttributeConverter<String, String> {
     override fun convertToEntityAttribute(dbData: String?): String? {
         if (dbData.isNullOrBlank()) return dbData
         try {
-            val cipher = createCipher(Cipher.DECRYPT_MODE, databaseEncryptionKey)
-            return decrypt(cipher, dbData)
+            val cipher = createCipher(Cipher.DECRYPT_MODE)
+            val encryptedBytes: ByteArray = Base64.getDecoder().decode(dbData.replace("\n", ""))
+            val decryptedBytes = cipher.doFinal(encryptedBytes)
+            return String(decryptedBytes)
         } catch (ex: GeneralSecurityException) {
             throw InternalException(ErrorCode.INT_ENCRYPTION, ex.localizedMessage)
         }
-    }
-
-    @Throws(IllegalBlockSizeException::class, BadPaddingException::class)
-    private fun encrypt(cipher: Cipher, attribute: String): String {
-        val bytesToEncrypt = attribute.toByteArray()
-        val encryptedBytes = cipher.doFinal(bytesToEncrypt)
-        return Base64.getEncoder().encodeToString(encryptedBytes)
-    }
-
-    @Throws(IllegalBlockSizeException::class, BadPaddingException::class)
-    private fun decrypt(cipher: Cipher, dbData: String): String {
-        val encryptedBytes: ByteArray = Base64.getDecoder().decode(dbData.replace("\n", ""))
-        val decryptedBytes = cipher.doFinal(encryptedBytes)
-        return String(decryptedBytes)
     }
 
     @Throws(
         InvalidAlgorithmParameterException::class, InvalidKeyException::class,
         NoSuchAlgorithmException::class, NoSuchPaddingException::class
     )
-    private fun createCipher(encryptionMode: Int, key: String): Cipher =
+    private fun createCipher(encryptionMode: Int): Cipher =
         Cipher.getInstance("AES/CBC/PKCS5Padding").apply {
-            val secretKey: Key = SecretKeySpec(key.toByteArray(), "AES")
+            val decodedKey = Base64.getDecoder().decode(databaseEncryptionKeyInBase64)
+            val secretKey: Key = SecretKeySpec(decodedKey, "AES")
             val algorithmParameters = IvParameterSpec(ByteArray(this.blockSize))
             init(encryptionMode, secretKey, algorithmParameters)
         }
