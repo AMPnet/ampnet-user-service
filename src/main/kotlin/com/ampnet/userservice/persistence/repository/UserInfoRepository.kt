4e7eea520ead8f2d54e2068532baf763aa0c39c5
Eugen Druzin <edruzin@gmail.com>
2021-04-02 14:26:52 +0200

    Handle multiple user info

--- src/main/kotlin/com/ampnet/userservice/persistence/repository/UserInfoRepository.kt
+++ src/main/kotlin/com/ampnet/userservice/persistence/repository/UserInfoRepository.kt
@@ -3,15 +3,14 @@ package com.ampnet.userservice.persistence.repository
 import com.ampnet.userservice.persistence.model.UserInfo
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import java.util.Optional
 import java.util.UUID
 
 interface UserInfoRepository : JpaRepository<UserInfo, UUID> {
-    fun findBySessionId(sessionId: String): Optional<UserInfo>
+    fun findBySessionIdOrderByCreatedAtDesc(sessionId: String): List<UserInfo>
 
     @Query(
         "SELECT userInfo FROM UserInfo userInfo INNER JOIN User user ON userInfo.uuid = user.userInfoUuid " +
-            "WHERE user.coop = :coop"
+            "WHERE user.coop = :coop AND userInfo.connected = true"
     )
-    fun findAllByCoop(coop: String): List<UserInfo>
+    fun findAllConnectedByCoop(coop: String): List<UserInfo>
 }
